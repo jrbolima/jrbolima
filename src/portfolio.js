bb1dc3b9077a608df444f17f19213207c5ef4a27
JR Bolima <gerardo.bolima@ctbcbank.com.ph>
2024-08-14 15:13:05 +0800

    Changes in all

--- src/portfolio.js
+++ src/portfolio.js
@@ -10,9 +10,13 @@ const about = {
   name: 'Gerardo Jr Bolima',
   role: 'Software Developer',
   description:
-    'Enthusiastic programmer eager to contribute to team success through hard work, ' +
-    'attention to detail and excellent organizational skills. Motivated to learn, ' +
-    'grow and excel in IT Industry while contributing my best to the organization I work for.',
+    'As an enthusiastic programmer, I am committed to driving team success ' +
+    'with my unwavering dedication to hard work and an exceptional eye to detail. ' +
+    'My strong organization skills enable me to tackle challeges efficiently, ' +
+    'ensuring that every project I touch is managed with precision. With a fervent desire to continuosly learn and grow, ' +
+    'I am posed to make significant contributions to the IT industry and deliver outstanding results for the organization I represent. ' +
+    'My motivation is not just to excel personally, but to elevate the collective performance of my team, ' +
+    'leveraging my skills to foster an environment of excellence.',
   resume: '', 
   social: {
     facebook: 'https://facebook.com/bolima22',
@@ -51,17 +55,51 @@ const skills = [
   'MSSQL',
   'Oracle',
   'PHP',
-  'HTML',
-  'CSS',
-  'Material UI',
+  'API',
+  'Kong',
+  'Web Services and Middlewares',
   'Git',
-  'Angular',
-  'React'
 ]
 
 const works = [
   // skills can be added or removed
-  // if there are no works experience, Works section won't show up
+  // if there are no works experience, Works section won't show up   
+  {
+    position: 'Systems Analyst/Programmer',
+    company: 'CTBC Bank (Philippines) Corp.',
+    yearsOfService: 'September 2023 - Present',
+    tasks: [
+      'Spearheaded API and Middleware Development as Technical Lead.',
+      'Drove Application Leadership for API Module and Retail Internet Banking Middleware.',
+      'Executed comprehensive Unit Testing to ensure robust software performance.',
+      'Facilitated User Acceptance Testing, providing support to Business users.',
+      'Developed SSIS Jobs tailored to business processes and application needs.'
+    ],
+  },
+  {
+    position: 'Software Developer',
+    company: 'H2 Software Consulting Services, Inc. (Staff Augmented at CTBC Bank (Philippines) Corp.)',
+    yearsOfService: 'March 2022 - September 2023',
+    tasks: [
+      'Expert in designing, building, and configuring applciations tailored to business requirements,',
+      'Skilled in customizaing ATM Software solutions to enhance business operations.',
+      'Proficient in executing comprehensice testing with ATM, VISA and BancNet transactions.',
+      'Conducted meticulous unit testing for applications and authored user manuals.',
+      'Assisted in developing test cases and facilitating User Accteptance Testing (UAT).',
+      'Collaborated with business users and testers to ensure technical compatibility and user satisfaction.',
+      'Managed ATM Switch configurations in test environments, including card creation and transaction log analysis.',
+      'Experiences in API development for seamless system integration.'
+    ],
+  },
+  {
+    position: 'Junior Programmer',
+    company: 'Covent Garden, Inc.',
+    yearsOfService: 'August 2019 - March 2022',
+    tasks: [
+      'Design a desktop applications for Accounting Department, e.g., Billing System, AP Monitoring System, Biometrics Login and Sales Invoice with printing',
+      'Assist employees whenever there are technical difficulties concerning computer hardware and software and assist senior developers.',
+    ],
+  },
   {
     position: 'IT Staff',
     company: 'Mitsubishi Hitachi Power System - Technical Services',
@@ -70,28 +108,7 @@ const works = [
       'Assist in defining requirements and designing applications to meet business process and application requirements as IT Staff for Software Development Team',
       'Assist Senior Developers.'
     ],
-  },
-  {
-    position: 'Junior Programmer',
-    company: 'Covent Garden, Inc.',
-    yearsOfService: 'August 2019 - March 2022',
-    tasks: [
-      'Design, build and configure desktop application to meet business process and application requirements.',
-      'Assist in defining requirements and designing applications to meet business process and application requirements as Jr. Programmer.',
-      'Assist employees whenever there are technical difficulties concerning computer hardware and software.',
-    ],
-  },
-  {
-    position: 'Software Developer',
-    company: 'H2 Software Consulting Services, Inc. (Project Hire at CTBC Bank Philippines)',
-    yearsOfService: 'March 2022 - Present',
-    tasks: [
-      'Design, modify and configure existing desktop application to meet business process and application requirements.',
-      'Configure ATM packages to meet business process requirements as ATM Support.',
-      'Conduct ATM Transaction Testing and BancNet Transactions when a new product has been defined.'
-    ],
-  },
-
+  }, 
 ]
 
 const contact = {
